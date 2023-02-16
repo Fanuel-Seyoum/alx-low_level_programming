b02d5c50fef7d7ccd8822358021c778e25b90bf3
fanuel-seyoum <fanuelseyoum@gmail.com>
2023-02-16 11:45:28 -0800

    the output in printed unix

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - an output of C program in unix
+ * Return: 1 (green)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return (1);
+}
+
