a920a656f57bb9b8991c857bd38ee4a808769dff
fanuel-seyoum <fanuelseyoum@gmail.com>
2023-02-16 10:32:04 -0800

    printing a txt using PUTS

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-/* this program simply prints a text and returns 0 */
-int main(void) /* this is the main function of the program */
+/**
+ * main -  the program prints a line 
+ * the program returns 0
+ */
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
