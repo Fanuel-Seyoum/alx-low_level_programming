9f3e0bb78231797ac080af8e6f74a70c8677d47c
fanuel-seyoum <fanuelseyoum@gmail.com>
2023-02-16 10:56:49 -0800

    finally with the betty thing

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - this program prints a file using printf
+ * Return: 0 (whenever successful)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+		return (0);
+}
