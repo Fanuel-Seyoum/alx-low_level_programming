5b684de30fd579b561ef314eaabe7c986f156927
fanuel-seyoum <fanuelseyoum@gmail.com>
2023-02-17 07:18:01 -0800

    number sign identifier

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+/* this programs tells if the number is positive, negative or zero*/
+
+/*
+ * main - the program tells the sign of an integer
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("is positive\n");
+	else if (n < 0)
+		printf("ïs zero\n");
+	else
+		printf("is negative\n");
+	/* your code goes there */
+	return (0);
+}
