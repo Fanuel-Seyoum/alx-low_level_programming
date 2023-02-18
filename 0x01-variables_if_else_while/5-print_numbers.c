ca6d9f9716f0ccbaecaef2c74ced4b7969babe21
fanuel-seyoum <fanuelseyoum@gmail.com>
2023-02-17 17:06:14 -0800

    list of numbers upto 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num_list;
+
+	num_list = 0;
+	while
+		(num_list <= 10)
+		{
+			printf(num_list'\n');
+	return (0);
+}
