434e7574c7d841bd8903fb663403ef54a7fdec46
fanuel-seyoum <fanuelseyoum@gmail.com>
2023-02-17 17:32:13 -0800

    list of integers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num_list;
+
+	for (num_list = 0; num_list <= 9; num_list++)
+		{
+			putchar(num_list);
+		}
+	putchar('\n');
+	return(0);
+}
