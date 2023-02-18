1c650652dab9bed7c9628e6cf9fa9284c2489e20
fanuel-seyoum <fanuelseyoum@gmail.com>
2023-02-17 16:39:19 -0800

    only lower case list

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+        char lower;
+
+        lower = 'a';
+
+	upper = 'A';
+
+        while
+                (lower <= 'z' && upper <= 'Z')
+                {
+                        putchar(lower);
+			putchar(upper);
+                        lower++;
+                }
+        putchar('\n');
+        return (0);
+}
