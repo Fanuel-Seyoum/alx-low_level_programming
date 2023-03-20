d386223db49b027b06ffd2fe297fc022ba42da44
Fanuel-Seyoum <108565144+Fanuel-Seyoum@users.noreply.github.com>
2023-03-20 23:13:19 +0300

    working on the first struct 0th question . . . listing the struct on the detail of my dog

---
+++ 0x0E-Structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - Listing the detail informationf of my dog
+ * @name: my pet's name
+ * @age: my pet's age
+ * @owner: My name, since I am the owner of my dog
+ *
+ * Description: The struct below is the detial information about my dog.
+ */
+
+struct dog{
+    char *name;
+    float *age;
+    char *owner;
+}
