09e80d94db6c5ee186f95d2507dc66bc1aaf1203
fanuel-seyoum <fanuelseyoum@gmail.com>
2023-03-21 05:56:19 -0700

    tenth commit

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -12,27 +12,14 @@
  * Return: Always 0.
  */
 
-struct dog
-{
-    *d.name;
-    char *name;
-    float age;
-    char *owner
-};
 
 void init_dog(struct dog *d, char* name, float age, char* owner)
 {
-   //Checking if the pointer is pointing to some value
    if (d == NULL)
    {
     printf("This is a NULL pointer");
     return;
    }
-
-//    if ((*d).name == NULL || (*d).owner == NULL) {
-//     printf("Error: Either NULL name or owner is passed");
-//     return;
-//    }
    (*d).name = name;
    (*d).age = age;
    (*d).owner = owner;
