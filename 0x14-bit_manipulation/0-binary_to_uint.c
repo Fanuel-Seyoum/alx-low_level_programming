24ee8bd0c27061b64c08cbcbaa376cb594d140f4
Fanuel-Seyoum <108565144+Fanuel-Seyoum@users.noreply.github.com>
2023-04-07 23:36:21 +0300

    main function added

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,23 +1,4 @@
-#include<stdlib.h>
-#include<stdio.h>
-unsigned int binary_to_uint(const char *b);
-void print_binary(unsigned long int n);
-
-int main(){
-   unsigned int n;
-
-    n = binary_to_uint("1");
-    printf("%u\n", n);
-    n = binary_to_uint("101");
-    printf("%u\n", n);
-    n = binary_to_uint("1e01");
-    printf("%u\n", n);
-    n = binary_to_uint("1100010");
-    printf("%u\n", n);
-    n = binary_to_uint("0000000000000000000110010010");
-    printf("%u\n", n);
-    return (0);
-}
+#include "main.h"
 
 unsigned int binary_to_uint(const char *b){
     int count = 0;
