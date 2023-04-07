4ca0b9ca41e4cd38cfc2d6cfd7809ef5e35d0d04
Fanuel-Seyoum <108565144+Fanuel-Seyoum@users.noreply.github.com>
2023-04-07 23:57:28 +0300

    new file added for print binary function

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+void print_binary(unsigned long int n){
+    int quotient = n;
+    int fold = 1;
+    int counter = 0;
+
+    while(1){ // counts how many folds we have to go until we reach the max fold less than the number we received
+        
+        if(fold+fold>quotient){
+            break;
+        }
+        counter++;
+        fold = fold+fold;
+    }
+    int newNum = quotient;
+    for(int i=counter;i>=0;i--){
+
+        if(i==counter){
+            if(quotient==0){
+                printf("0");
+            }else{
+                printf("1");
+            }
+            newNum = newNum - fold;
+            fold = fold >> 1;
+            continue;
+        }
+        if(newNum<fold){
+            printf("0");
+        }else{
+            printf("1");
+            newNum = newNum - fold;
+        }
+    
+        fold = fold >> 1;
+
+    }
+}
+
