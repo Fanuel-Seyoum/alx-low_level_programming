20f6af208dd374737ca921bdc7628b20a03a011b
Fanuel-Seyoum <108565144+Fanuel-Seyoum@users.noreply.github.com>
2023-04-07 23:11:08 +0300

    second commit second function

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,27 +1,44 @@
 #include<stdlib.h>
 #include<stdio.h>
 unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
 
 int main(){
-   unsigned int x = binary_to_uint("101");
-   printf("\n%d \n",x);
+   unsigned int n;
+
+    n = binary_to_uint("1");
+    printf("%u\n", n);
+    n = binary_to_uint("101");
+    printf("%u\n", n);
+    n = binary_to_uint("1e01");
+    printf("%u\n", n);
+    n = binary_to_uint("1100010");
+    printf("%u\n", n);
+    n = binary_to_uint("0000000000000000000110010010");
+    printf("%u\n", n);
+    return (0);
 }
 
 unsigned int binary_to_uint(const char *b){
-    int count;
-	count = 0;
-
-	while (b[count] != '\0')   //counts the numbers of characters in the pointer argument
-	{
-        printf("%c",b[count]);
-        count++;	
-	}
+    int count = 0;
+    int wrongChar = 0;
     int result = 0;
     int twoMultiples;
     int charH;
 
+	while (b[count] != '\0')   //counts the numbers of characters in the pointer argument
+	{
+        count++;	
+	}
+    
+
     for(int i=0;i<count;i++){  //interates through the char array 
         twoMultiples = 1;
+
+        if(b[i]!='0'&&b[i]!='1'){
+            wrongChar = 1;
+            break;
+        }
 
         if(b[i]!=0){  // it prevents the loop from functioning if the digit is only zero
             for(int j=(count-i);j>1;j--){ // handles the multiplication  of the two powers
@@ -32,5 +49,46 @@ unsigned int binary_to_uint(const char *b){
         result = result + (charH*twoMultiples);
     }
 
-    return result;
+    if(wrongChar==0){
+        return result;
+    } else{
+        return 0;
+    }
 }
+
+void print_binary(unsigned long int n){
+    int quotient = n;
+    int fold = 1;
+    int counter = 0;
+
+    while(1){ // counts how many folds we have to go until we reach the max fold less than the number we received
+        
+        if(fold+fold>quotient){
+            break;
+        }
+        counter++;
+        fold = fold+fold;
+    }
+    int newNum = quotient;
+    for(int i=counter;i>=0;i--){
+
+        if(i==counter){
+            printf("1");
+            newNum = newNum - fold;
+            fold = fold >> 1;
+            continue;
+        }
+        if(newNum<fold){
+            printf("0");
+        }else{
+            printf("1");
+            newNum = newNum - fold;
+        }
+    
+        fold = fold >> 1;
+
+    }
+}
+
+
+
---
+++ 0x14-bit_manipulation/tempCodeRunnerFile.c
@@ -0,0 +1,4 @@
+if(b[i]!=0&&b[i]!=1){
+        //     wrongChar = 1;
+        //     break;
+        // }
