20c016c2ea8a9553cf78ada5418341ff68b578fc
Fanuel-Seyoum <108565144+Fanuel-Seyoum@users.noreply.github.com>
2023-04-10 16:12:24 +0300

    my code standard library

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,29 +3,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-unsigned int binary_to_uint(const char *b)
-{
- unsigned int res = 0;
- int base = 1, i = 0;
+unsigned int binary_to_uint(const char *b){
+    int count = 0;
+    int wrongChar = 0;
+    int result = 0;
+    int twoMultiples;
+    int charH;
 
- if (b == NULL)
-  return (0);
+    if(b==NULL){
+        return 0;
+    }
 
- while (b[i + 1])
- {
-  if (b[i] != '0' && b[i] != '1')
-   return (0);
-  i++;
- }
+	while (b[count] != '\0')   //counts the numbers of characters in the pointer argument
+	{
+        count++;	
+	}
+    
 
- while (i >= 0)
- {
-  res += ((b[i] - '0') * base);
-  base *= 2;
-  i--;
- }
+    for(int i=0;i<count;i++){  //interates through the char array 
+        twoMultiples = 1;
 
+        if(b[i]!='0'&&b[i]!='1'){
+            wrongChar = 1;
+            break;
+        }
 
- return (res);
+        if(b[i]!=0){  // it prevents the loop from functioning if the digit is only zero
+               twoMultiples =  1<<(count-i-1);
+        }
+        charH = b[i]-'0';  //conversion of char to int
+        result = result + (charH*twoMultiples);
+    }
 
+    if(wrongChar==0){
+        return result;
+    } else{
+        return 0;
+    }
 }
+
+
+
+
