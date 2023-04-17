1f276f8ad61107c4be4bbb3dc135387e48bc66d5
Fanuel-Seyoum <108565144+Fanuel-Seyoum@users.noreply.github.com>
2023-04-17 13:58:06 +0300

    testing the second optimization

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,22 +15,24 @@
 
 int create_file(const char *filename, char *text_content)
 {
-    int openfd;
-    ssize_t writecounter, len;
+    int openfd, writecounter, len;
 
 	if (filename == NULL)
 		return (-1);
 
+    if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
 	openfd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-	if (openfd < 0)
-		return (-1);
+    
+    writecounter = write(openfd, text_content, len);
 
-	while (text_content && *(text_content + len))
-		len++;
-
-	writecounter = write(openfd, text_content, len);
+    if (fd < 0 || writecounter < 0)
+    {
+        return (-1);
+    }
 	close(openfd);
-	if (writecounter < 0)
-		return (-1);
 	return (1);
 }
