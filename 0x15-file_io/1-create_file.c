292d3db7532ae1fc2b74aa459e3ee378fac5f098
Fanuel-Seyoum <108565144+Fanuel-Seyoum@users.noreply.github.com>
2023-04-17 14:05:33 +0300

    checking if macbook passes

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,9 @@
-#include <fcntl.h>
+#include <stdio.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
 #include <stdlib.h>
+#include "main.h"
 
 int create_file(const char *filename, char *text_content) {
     if (filename == NULL) {
