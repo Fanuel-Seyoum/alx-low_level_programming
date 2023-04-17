813ed524de19d35e779b481bca8a9878a2cec7da
Fanuel-Seyoum <108565144+Fanuel-Seyoum@users.noreply.github.com>
2023-04-17 14:04:14 +0300

    checking if macbook passes

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,38 +1,25 @@
-#include <stdio.h>
-#include <unistd.h>
 #include <fcntl.h>
-#include <sys/stat.h>
+#include <unistd.h>
 #include <stdlib.h>
-#include "main.h"
 
-/**
- * read_textfile - a function that creates a text file and prints to POSIX STDOUT
- * @filename: The filename to be opened
- * @letters: The number of letters to be read and print
- * Return: The number of letters read and printed, or 0 on failure
- */
-
-
-int create_file(const char *filename, char *text_content)
-{
-    int openfd, writecounter, len;
-
-	if (filename == NULL)
-		return (-1);
-
-    if (text_content != NULL)
-	{
-		for (len = 0; text_content[len];)
-			len++;
-	}
-	openfd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
-    
-    writecounter = write(openfd, text_content, len);
-
-    if (openfd < 0 || writecounter < 0)
-    {
-        return (-1);
+int create_file(const char *filename, char *text_content) {
+    if (filename == NULL) {
+        return -1;
     }
-	close(openfd);
-	return (1);
+    int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+    if (fd == -1) {
+        return -1;
+    }
+    if (text_content != NULL) {
+        int len = 0;
+        while (text_content[len] != '\0') {
+            len++;
+        }
+        if (write(fd, text_content, len) != len) {
+            close(fd);
+            return -1;
+        }
+    }
+    close(fd);
+    return 1;
 }
